42036051ace5568f151d8d7f74adb7bfd8572add
Leoni <118344314+lktay@users.noreply.github.com>
2023-01-19 14:21:31 +0000

    comment out broken code

--- API Website/app.js
+++ API Website/app.js
@@ -26,21 +26,21 @@ function generateCocktail(e) {
   const cocktailName = cocktail.strDrink;
   header.innerHTML = `<h1>${cocktailName}</h1>`;
 
-  const getIngredients = Object.keys(cocktail)
-    .filter(function (ingredient) {
-      return ingredient.indexOf("strIngredient") == 0;
-    })
-    .reduce(function (ingredients, ingredient) {
-      if (cocktail[ingredient] != null) {
-        ingredients[ingredient] = cocktail[ingredient];
-      }
-      return ingredients;
-    }, {});
+  // const getIngredients = Object.keys(cocktail)
+  //   .filter(function (ingredient) {
+  //     return ingredient.indexOf("strIngredient") == 0;
+  //   })
+  //   .reduce(function (ingredients, ingredient) {
+  //     if (cocktail[ingredient] != null) {
+  //       ingredients[ingredient] = cocktail[ingredient];
+  //     }
+  //     return ingredients;
+  //   }, {});
 
-  for (let key in getIngredients) {
-    let value = getIngredients[key];
-    listItem = document.createElement("li");
-    listItem.innerHTML = value;
-    ingredientsList.appendChild(listItem);
-  }
+  // for (let key in getIngredients) {
+  //   let value = getIngredients[key];
+  //   listItem = document.createElement("li");
+  //   listItem.innerHTML = value;
+  //   ingredientsList.appendChild(listItem);
+  // }
 }
