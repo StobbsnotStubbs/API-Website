fcc2a4fe98df7a64351ff154704264657cffeda7
Leoni <118344314+lktay@users.noreply.github.com>
2023-01-19 14:15:15 +0000

    test kevin pls look

--- API Website/app.js
+++ API Website/app.js
@@ -17,12 +17,30 @@ function generateCocktail(e) {
     .then(() => console.log(cocktail))
 
     .catch((error) => console.error("FETCH ERROR:", error));
-  const cocktailName = cocktail.strDrink;
 
+  //elements on the DOM
   header = document.querySelector(".recipe");
   ingredientsList = document.querySelector(".ingredients");
   instructions = document.querySelector(".instructions");
 
+  const cocktailName = cocktail.strDrink;
   header.innerHTML = `<h1>${cocktailName}</h1>`;
-  ingredientsList.createElement;
+
+  const getIngredients = Object.keys(cocktail)
+    .filter(function (ingredient) {
+      return ingredient.indexOf("strIngredient") == 0;
+    })
+    .reduce(function (ingredients, ingredient) {
+      if (cocktail[ingredient] != null) {
+        ingredients[ingredient] = cocktail[ingredient];
+      }
+      return ingredients;
+    }, {});
+
+  for (let key in getIngredients) {
+    let value = getIngredients[key];
+    listItem = document.createElement("li");
+    listItem.innerHTML = value;
+    ingredientsList.appendChild(listItem);
+  }
 }
