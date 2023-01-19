e4b31a4f55750f1bcb89905321633b00db20a566
Leoni <118344314+lktay@users.noreply.github.com>
2023-01-19 14:04:38 +0000

    fetch in click event listener, trial and error on ing list

--- API Website/app.js
+++ API Website/app.js
@@ -2,22 +2,27 @@ const generateBtn = document.querySelector(".generate");
 
 let randomCocktail = "http://www.thecocktaildb.com/api/json/v1/1/random.php";
 
-fetch(randomCocktail)
-  .then((response) => {
-    if (response.ok) {
-      return response.json();
-    } else {
-      throw new Error("NETWORK RESPONSE ERROR");
-    }
-  })
-  .then((data) => (cocktail = data))
-  .then(() => console.log(cocktail))
-
-  .catch((error) => console.error("FETCH ERROR:", error));
-
 generateBtn.addEventListener("click", generateCocktail);
 
 function generateCocktail(e) {
-  const name = cocktail.drinks[0].strDrink;
-  document.querySelector(".recipe").innerHTML = `<h1>${name}</h1>`;
+  fetch(randomCocktail)
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error("NETWORK RESPONSE ERROR");
+      }
+    })
+    .then((data) => (cocktail = data.drinks[0]))
+    .then(() => console.log(cocktail))
+
+    .catch((error) => console.error("FETCH ERROR:", error));
+  const cocktailName = cocktail.strDrink;
+
+  header = document.querySelector(".recipe");
+  ingredientsList = document.querySelector(".ingredients");
+  instructions = document.querySelector(".instructions");
+
+  header.innerHTML = `<h1>${cocktailName}</h1>`;
+  ingredientsList.createElement;
 }
