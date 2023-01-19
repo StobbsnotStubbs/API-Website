b3150dc6ae0bd37684f626817a7ea38b85118d8a
Kevin Stobbs <Kevin.Stobbs94@gmail.com>
2023-01-19 13:52:17 +0000

    Now refreshes on click

--- API Website/app.js
+++ API Website/app.js
@@ -2,22 +2,21 @@ const generateBtn = document.querySelector(".generate");
 
 let randomCocktail = "http://www.thecocktaildb.com/api/json/v1/1/random.php";
 
-fetch(randomCocktail)
-  .then((response) => {
-    if (response.ok) {
-      return response.json();
-    } else {
-      throw new Error("NETWORK RESPONSE ERROR");
-    }
-  })
-  .then((data) => (cocktail = data))
-  .then(() => console.log(cocktail))
-
-  .catch((error) => console.error("FETCH ERROR:", error));
-
 generateBtn.addEventListener("click", generateCocktail);
 
 function generateCocktail(e) {
+  fetch(randomCocktail)
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error("NETWORK RESPONSE ERROR");
+      }
+    })
+    .then((data) => (cocktail = data))
+    .then(() => console.log(cocktail))
+
+    .catch((error) => console.error("FETCH ERROR:", error));
   const name = cocktail.drinks[0].strDrink;
   document.querySelector(".recipe").innerHTML = `<h1>${name}</h1>`;
 }
