4f4f2b155ce13f7f87a5f20611fd1d294721ec6f
Leoni <118344314+lktay@users.noreply.github.com>
2023-01-19 13:11:55 +0000

    Added button that generates drink name on click

--- API Website/app.js
+++ API Website/app.js
@@ -1,9 +1,23 @@
+const generateBtn = document.querySelector(".generate");
+
 let randomCocktail = "http://www.thecocktaildb.com/api/json/v1/1/random.php";
 
 fetch(randomCocktail)
-  .then((response) => response.json())
-  .then((data) => (responseData = data))
-  .then(() => {
-    console.log(responseData);
-    document.querySelector("p").innerHTML = responseData.strDrink;
-  });
+  .then((response) => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw new Error("NETWORK RESPONSE ERROR");
+    }
+  })
+  .then((data) => (cocktail = data))
+  .then(() => console.log(cocktail))
+
+  .catch((error) => console.error("FETCH ERROR:", error));
+
+generateBtn.addEventListener("click", generateCocktail);
+
+function generateCocktail(e) {
+  const name = cocktail.drinks[0].strDrink;
+  document.querySelector(".recipe").innerHTML = `<h1>${name}</h1>`;
+}
